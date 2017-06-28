495eeb420bd15d69c44b9050e16dc475700c9db4
Laurent Sarrazin <laurent.sarrazin@edf.fr>
2017-06-28 22:59:42 +0200

    Added more HKey ATDD & a few syntax hints on HKey

--- src/main/scala/org/edma/hbase/HKey.scala
+++ src/main/scala/org/edma/hbase/HKey.scala
@@ -1,10 +1,13 @@
 package org.edma.hbase
 
+import collection.immutable.SortedSet
+
 object KeyType extends Enumeration {
   type KeyType = Value
   val Single, Multiple, Range, Pattern, Invalid = Value
 }
 import KeyType._
+
 
 abstract class HKey {
 
@@ -14,25 +17,43 @@ abstract class HKey {
   def isMultiple: Boolean = (ktype == Multiple)
   def isRange: Boolean = (ktype == Range)
   def isPattern: Boolean = (ktype == Pattern)
+  def isInvalid: Boolean = (ktype == Invalid)
 
-  def to(ev: String): HKey
-  
+  def to(ev: String): HKey = HInvalidKey
+  def +(nv: String): HKey = HInvalidKey
+  def ++(nvs: Iterable[String]): HKey = HInvalidKey
+
   def patternToKey(pv: String): String = 
-    if (pv.endsWith("%")) pv.substring(0, pv.length() - 1)
+    if (isPattern(pv)) pv.substring(0, pv.length() - 1)
     else pv
+
+  def isPattern(pv: String): Boolean = pv.endsWith("%")
+    
+  def addKeyToSet(vs: SortedSet[String], nv: String): SortedSet[String] = {
+    (vs + nv)
+  }
+
+  def addKeysToSet(vs: SortedSet[String], nvs: Iterable[String]): SortedSet[String] = {
+    (vs ++ nvs)
+  }
 
 }
 
 object HInvalidKey extends HKey {
   def ktype: KeyType = Invalid
   
-  def to(ev: String): HKey = this
+  override def toString = "Invalid"
 }
 
 class HSingleKey(kv: String) extends HKey {
   def ktype: KeyType = Single
 
-  def to(ev: String): HKey = new HRangeKey(kv, ev)
+  override def to(ev: String): HKey = 
+    if (isPattern(ev)) HInvalidKey
+    else new HRangeKey(kv, ev)
+  
+  override def +(nv: String): HKey = new HMultipleKey(SortedSet(kv)) + nv
+  override def ++(nvs: Iterable[String]): HKey = new HMultipleKey(SortedSet(kv)) ++ nvs
 
   override def toString = f"""S"${kv}""""
 }
@@ -40,38 +61,65 @@ class HSingleKey(kv: String) extends HKey {
 class HPatternKey(kv: String) extends HKey {
   def ktype: KeyType = Pattern
   
-  def to(ev: String): HKey = new HRangeKey(patternToKey(kv), ev)
+  override def to(ev: String): HKey = {
+    val sv = patternToKey(kv)
+    if (sv < ev) new HRangeKey(patternToKey(kv), ev)
+    else HInvalidKey
+  }
 
   override def toString = f"""P"${kv}""""
 }
 
-class HMultipleKey(kvs: Iterable[String]) extends HKey {
+class HMultipleKey(kvs: SortedSet[String]) extends HKey {
   def ktype: KeyType = Multiple
-
-  //TODO: Handle properly
-  def to(ev: String): HKey = HInvalidKey
+  
+  override def to(ev: String): HKey = HInvalidKey
+  
+  override def +(nv: String): HMultipleKey = new HMultipleKey(addKeyToSet(kvs, nv))
+  override def ++(nvs: Iterable[String]): HMultipleKey = new HMultipleKey(addKeysToSet(kvs, nvs.toSet))
   
   override def toString = "M" + kvs.mkString("[\"", "\",\"", "\"]")
 
 }
 
 class HRangeKey(kvs: String, kve: String) extends HKey {
-  
   def ktype: KeyType = Range
-  def to(ev: String): HKey = HInvalidKey
-  
   override def toString = f"""R["${kvs}" -> "${kve}"]"""
+  
+  override def to(ev: String): HKey = {
+    if (ev >= kve) new HRangeKey(kvs, ev)
+    else HInvalidKey
+  }
 }
+
+//TODO: Add hybrid key
+/* An hybrid key can hold keys from Multiple and Range patterns 
+ * For example :
+ * val key = HKey("12345", "345%", "67890")
+ * */
 
 object HKey {
 
+  def from(kv: String): HKey = HKey(kv)
+  def :=(kv: String): HKey = HKey(kv)
   def apply(kv: String): HKey = {
-    if (kv.endsWith("%")) new HPatternKey(kv)
+    if (kv contains '%') {
+      if (kv endsWith "%") new HPatternKey(kv)
+      else HInvalidKey
+    }
     else new HSingleKey(kv)
   }
 
+  def :=(kvs: Iterable[String]): HKey = HKey(kvs)
   def apply(kvs: Iterable[String]): HKey = {
-    new HMultipleKey(kvs)
+    val sset = SortedSet() ++ kvs
+    new HMultipleKey(sset)
   }
 
+  def :=(vs: (String, String)): HKey = HKey(vs)
+  def apply(vs: (String, String)): HKey = HKey(List(vs._1, vs._2))
+  
+  def apply(vs: (String, String, String)): HKey = HKey(List(vs._1, vs._2, vs._3))
+  def apply(vs: (String, String, String, String)): HKey = HKey(List(vs._1, vs._2, vs._3, vs._4))
+  def apply(vs: (String, String, String, String, String)): HKey = HKey(List(vs._1, vs._2, vs._3, vs._4, vs._5))
 }
--- src/test/scala/org/edma/hbase/HKeySpec.scala
+++ src/test/scala/org/edma/hbase/HKeySpec.scala
@@ -2,31 +2,109 @@ package org.edma.hbase
 
 import org.scalatest._
 
-class HKeySpec extends FeatureSpec with GivenWhenThen {
-  
-  info("As a HTools user")
-  info("I want to be able create & mutate keys to HBase")
-  info("So I can express commands from the REPL")
+class HKeySpec extends FreeSpec {
 
-  feature("HKey creations") {
-    scenario("User creates a single key") {
-      Given("nothing")
-      When("the user declares a key with a single value")
-      val key = HKey("123")
-      Then("the key is a SingleKey")
-      assert(key isSingle)
-    }
-  }
+  "A Key" - {
 
-  feature("HKey mutations") {
-    scenario("User appends a range value to a single key") {
-      Given("a single key")
-      val key = HKey("123")
-      assert(key isSingle)
-      When("the user calls the to method")
-      val nkey = key to "789"
-      Then("the resulting key is a range")
-      assert(nkey isRange)
+    "created from a single value (eg \"Hello\")" - {
+
+      val skey = HKey("Hello")
+
+      "produces an HSingleKey instance" in {
+        assert(skey.isSingle)
+        assert(skey.toString === "S\"Hello\"")
+      }
+      "mutates to a range key using to operator" in {
+        val skey2 = skey to "World"
+        assert(skey2.isRange)
+        assert(skey2.toString === "R[\"Hello\" -> \"World\"]")
+      }
+      "mutates to a multiple key using + operator" in {
+        val skey3 = skey + "World"
+        assert(skey3.isMultiple)
+        assert(skey3.toString === "M[\"Hello\",\"World\"]")
+      }
+      "mutates to a multiple key using ++ operator" in {
+        val skey4 = skey ++ List("Bonjour", "Monde")
+        assert(skey4.isMultiple)
+        assert(skey4.toString === "M[\"Bonjour\",\"Hello\",\"Monde\"]")
+      }
     }
+
+    "created as a multiple value" - {
+
+      val skey = HKey("Hello")
+
+    }
+
+    "created from a pattern value like \"Hello%\"" - {
+
+      val pkey = HKey("Hello%")
+
+      "produces an HPatternKey instance" in {
+        assert(pkey.isPattern)
+        assert(pkey.toString === "P\"Hello%\"")
+      }
+      "unless '%' found before last char" in {
+        val wkey1 = HKey("%Hello")
+        assert(wkey1.isInvalid)
+        val wkey2 = HKey("Hel%lo")
+        assert(wkey1.isInvalid)
+      }
+      "mutates to a range key using to operator" - {
+
+        "if new bound is after pattern" in {
+            val pkey2 = pkey to "World"
+            assert(pkey2.isRange)
+            assert(pkey2.toString === "R[\"Hello\" -> \"World\"]")
+        }
+        
+        "but fails if new bound is before pattern" in {
+          val pkey3 = pkey to "A"
+          assert(pkey3.isInvalid)
+          assert(pkey3 == HInvalidKey)
+          assert(pkey3.toString === "Invalid")
+        }
+      }
+    }
+
+    "created as a range key" - {
+      val rkey = HKey from ("ABC") to ("DEF")
+      val rkey2 = rkey to "XYZ" //> rkey3  : org.edma.hbase.HKey = Invalid
+      val rkey4 = rkey to "BAA" //> rkey4  : org.edma.hbase.HKey = Invalid
+      
+      "produces a range key" in {
+        assert(rkey.isRange)
+        assert(rkey.toString === "R[\"ABC\" -> \"DEF\"]")
+      }
+      "extends to a new range" - {
+        
+        "when extension is after upper bound" in {
+          val rkey2 = rkey to "XYZ"
+          assert(rkey2.isRange)
+          assert(rkey2.toString === """R["ABC" -> "XYZ"]""")
+        }
+        "but fails otherwise" in {
+          val rkey3 = rkey to "BCD"
+          assert(rkey3.isInvalid)
+          assert(rkey3 == HInvalidKey)
+          assert(rkey3.toString === "Invalid")
+        }
+      }
+    }
+
+    val mkey = HKey(List("ABC", "DEF")) //> mkey  : org.edma.hbase.HKey = M["ABC","DEF"]
+    val mkeyd = HKey(List("789", "456", "123", "456", "789")) //> mkeyd  : org.edma.hbase.HKey = M["123","456","789"]
+    val mkeyo = HKey(List("789", "456", "123")) //> mkeyo  : org.edma.hbase.HKey = M["123","456","789"]
+    val mkeyo2 = mkeyo + "456" //> mkeyo2  : org.edma.hbase.HKey = M["123","456","789"]
+
+    val mkey2 = mkey to "XYZ" //> mkey2  : org.edma.hbase.HKey = Invalid
+    val mkey3 = mkey + "XYZ" //> mkey3  : org.edma.hbase.HKey = M["ABC","DEF","XYZ"]
+    val mkey4 = mkey3 ++ List("GHI", "JKL") //> mkey4  : org.edma.hbase.HKey = M["ABC","DEF","GHI","JKL","XYZ"]
+
+    val tkey = HKey("123", "456") //> tkey  : org.edma.hbase.HKey = M["123","456"]
+    val tkeyd = HKey("789", "456", "123", "456", "789")
+    //> tkeyd  : org.edma.hbase.HKey = M["123","456","789"]  // Describe a scope for a subject, in this case: "A Set"
+
   }
 }
